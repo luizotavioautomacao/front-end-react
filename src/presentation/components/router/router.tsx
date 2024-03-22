f599d3bba85a0902441cd9c26dff3391c85d56fe
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-03-21 21:30:12 -0300

    feat: react-dom [incomplete]

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,16 @@
+// import { Login } from "@/presentation/pages";
+// import React from "react";
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+// const Spinner: React.FC = () => {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/login" Component={Login}/>
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default Spinner;
